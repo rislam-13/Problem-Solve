a5ddc1e40ee19703edc026e8420866ac7aac5bcd
Reazul Islam <40799648+rislam-13@users.noreply.github.com>
2018-08-15 11:28:45 +0600

    Create Problem 4.cpp

---
+++ CP book subeen/Problem 4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, i, a, b, c;
+    cin>>n;
+
+    for(i=0; i<n; i++)
+    {
+        cin>>a>>b>>c;
+        cout<<a*b*c<<endl;
+    }
+
+    return 0;
+}
